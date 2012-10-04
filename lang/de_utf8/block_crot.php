f6b1d3fe6504f63bc4f5f5772216b5e089f3e5c9
Sergey Butakov <butakov@noplagiarism.noplagiarism.org>
2012-10-04 06:47:59 +0400

    initail commit of crot for moodle 1.9.x

---
+++ lang/de_utf8/block_crot.php
@@ -0,0 +1,65 @@
+<?php
+
+
+$string['block_name'] = 'Plagiatsprüfung';
+$string['course_summary'] = 'Kurszusammenfassung';
+$string['Topics'] = 'Themen';
+$string['report'] = 'Bericht';
+$string['settings'] = 'Einstellungen';
+$string['have_to_be_a_teacher'] = 'Nur Trainer können diesen Block sehen';
+$string['assignments']='Aufgaben';
+$string['local']='Lokal';
+$string['global']='Global';
+$string['settings']='Einstellungen';
+$string['settings_cancelled']='Die Einstellungen der Plagiatsprüfung sind abgebrochen worden';
+$string['settings_saved']='Die Einstellungen der Plagiatsprüfung wurden gespeichert';
+$string['save']='Speichern';
+$string['select_assignment']='Aufgabe auswählen';
+$string['student_name']='Name des Lerners';
+$string['similar']='Ähnliche Aufgaben';
+$string['default_threshold']='Voreinstellung der Schwelle';
+$string['grammar_size']='Grammatik Größe';
+$string['colours']='Farben';
+$string['window_size']='Fenstergröße';
+$string['cluster_distance']='Max Abstand zwischen hashes im Textcluster';
+$string['cluster_size']='Minimum Clustergröße';
+$string['global_search_threshold']='Globale Such-Schwelle';
+$string['default_threshold']='Voreingestellte Schwelle';
+$string['global_search_settings']='Globale Sucheinstellungen';
+$string['global_search_threshold']='Globale Such-Schwelle';
+$string['MS_live_key']='MS Application ID key';
+$string['global_search_query_size']='Globale Suchanfragen-Größe';
+$string['percentage_of_search_queries']='Prozentsatz der Suchanfragen für Web-Suche (1-100)';
+$string['number_of_web_documents']='Anzah der Webdokumente, die heruntergeladen werden sollen(1=<)';
+$string['clean_tables']='Tabellen säubern (<b>ACHTUNG!</b><br> Entfernt alle Crot-Daten außer der Aufgaben die zur Überprüfung eingestellt wurden.<br>Neuberechnung der Fingerabdrücke kann hohe Serverlast verursachen)';
+$string['culture_info']='Kultur-Info für globale Suche';
+$string['antiword_path']='Pfad zu antiword<br/>Leer lassen, wenn antiword nicht benutzt werden soll <br/>um MS-Word-Einreichungen zu verarbeiten';
+$string['tools']='Tools';
+$string['test_global_serach']='Ankreuzen um einen Schnelltest der globalen Suche durchzuführen';
+
+
+
+$string['download_inwicast_message'] = '<p>INWICAST Publisher is a very simple and powerful tool designed to record and publish podcasts, videocasts, screencasts and slidecasts for the web and for mobile devices. INWICAST Publisher allows you to:</p><p>
+
+    * record podcasts, videocasts, screencasts and Powerpoint slidescasts<br/>
+    * convert recorded medias to various formats: flv, wmv, mp4, mp3, etc.<br/>
+    * create multimedia content for mobile players like iPod or Zune<br/>
+    * create and manage multimedia playlists<br/>
+    * easily publish your podcasts on your Moodle platform<br/>
+</p>';
+
+$string['inwicast_tuto_message'] = '<img src="images/tutorial/tuto.jpg" /><br>'
+					.'<table width="80%">'
+					.'<tr><td><img src="images/tutorial/ca.gif" /></td>'
+					.'<td align="left"><small>Zone d\'écran : plein écran.<br>Images par secondes : 5.</small></td></tr>'
+					.'<tr><td><img src="images/tutorial/micro.gif" /></td>'
+					.'<td align="left"><small>Microphone : sélectionnez votre micro.<br>Qualité audio : moyenne.</small></td></tr>'
+					.'<tr><td><img src="images/tutorial/video.gif" /></td>'
+					.'<td align="left"><small>Vidéo : sélectionnez votre webcam.<br>Qualité audio : moyenne Conversion : Flash - Wmv</small></td></tr>'
+					.'<tr><td><img src="images/tutorial/serveur.gif" /></td><td align=\"left\">'
+					.'<small>Attention, si vous devez enchaîner plusieurs enregistrements, il est conseillé de choisir l\'option "Travailler hors connexion" et de publier plus tard.</small></td></tr>'
+					. '</table>'
+					. '</td></tr>';
+$string['FILE_FORMAT_NOT_ALLOWED'] = "Dieses Dateiformat ist leider nicht zum Upload zugelassen";
+
+?>
